a0d8980d187a79e9f4d079866f7432dad5c22129
dahalsulav <sulavd7@email.com>
2022-02-22 13:03:22 +0545

    first commit

---
+++ store/tests/test_models.py
@@ -0,0 +1,47 @@
+from turtle import title
+from django.test import TestCase
+from django.contrib.auth.models import User
+from store.models import Category, Product
+
+class TestCategoriesModel(TestCase):
+    def setUp(self):
+        self.data1 = Category.objects.create(name="django",slug="django")
+    
+    def test_category_model_entry(self):
+        '''
+        Test category model data insertion/types/field attributes
+        '''
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+    
+    def test_category_model_entry(self):
+        '''
+        Test category model default name
+        '''
+        data = self.data1
+        self.assertEqual(str(data),'django')
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name="django",slug="django")
+        User.objects.create(username="admin")
+        self.data1 = Product.objects.create(category_id=1,title='Django For APIS',slug='django-for-apis',created_by_id=1,price='99.00',image="django_for_api")
+
+    def test_product_model_entry(self):
+        '''
+        Test product model data insertion/types/field attributes
+        '''
+        data = self.data1
+        self.assertTrue(isinstance(data,Product))
+    
+    def test_product_model_entry(self):
+        '''
+        Test product model default name
+        '''
+        data = self.data1
+        self.assertTrue(str(data),"Django For APIS")
+
+
+    
+    
+        
